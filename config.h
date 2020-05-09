437bd2c49e187b62a01cfbe188b94b636b77e6b3
Vlad Doster <mvdoster@gmail.com>
2020-05-09 18:31:30 -0500

    Update config.h

--- config.h
+++ config.h
@@ -4,12 +4,12 @@ static const Block blocks[] = {
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
+	/* {"",	"news",		0,	6},  */
 	/* {"",	"crypto",	0,	13}, */
-	{"",	"torrent",	20,	7},
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	13}, */
-	/* {"",	"moonphase",	18000,	5}, */
+	/* {"",	"torrent",	20,	7},  */
+	{"",	"memory",	10,	14},
+	 {"",	"cpu",		10,	13},
+	/* {"",	"moonphase",	18000,	5},  */
 	{"",	"weather",	18000,	5},
 	{"",	"mailbox",	180,	12},
 	/* {"",	"nettraf",	1,	16}, */
@@ -17,7 +17,7 @@ static const Block blocks[] = {
 	{"",	"battery | tr \'\n\' \' \'",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	/* {"",	"help-icon",	0,	15}, */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

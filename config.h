ca6dbad5c71c976a8e3c15160f88b03e4d15ce5e
Vlad Doster <mvdoster@gmail.com>
2020-05-09 19:20:41 -0500

    Update config.h

--- config.h
+++ config.h
@@ -3,19 +3,19 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
 	/* {"",	"news",		0,	6},  */
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"torrent",	20,	7},  */
 	/* {"",	"moonphase",	18000,	5},  */
-	{"",	"weather",	18000,	5},
 	/* {"",	"mailbox",	180,	12}, */
+	{"",	"pacpackages",	0,	8},
+	{"",	"weather",	18000,	5},
+	{"",	"volume",	0,	10},
+	{"",	"battery | tr \'\n\' \' \'",	5,	3},
 	{"",	"memory",	10,	14},
 	{"",	"cpu",		10,	13},
 	{"",	"nettraf",	1,	16},
 	{"",	"internet",	5,	4},
-	{"",	"volume",	0,	10},
-	{"",	"battery | tr \'\n\' \' \'",	5,	3},
 	{"",	"clock",	60,	1},
 };
 

c8459091ff7029a793480c1fb9354a8d8c2269d4
Vlad Doster <mvdoster@gmail.com>
2020-05-12 02:38:27 -0500

    Update config.h

--- config.h
+++ config.h
@@ -1,23 +1,15 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"weather",	18000,	5},
-	{"",	"volume",	0,	10},
+	{"",	"pacpackages",			0,	8},
+	{"",	"network_traffic",		1,	16},
 	{"",	"battery | tr \'\n\' \' \'",	5,	3},
-	{"",	"memory",	10,	14},
-	{"",	"cpu",		10,	13},
-	{"",	"nettraf",	1,	16},
-	{"",	"internet",	5,	4},
-	{"",	"clock",	60,	1},
+	{"",	"memory",			10,	14},
+	{"",	"cpu",				10,	13},
+	{"",	"internet_connection",		5,	4},
+	{"",	"volume",			0,	10},
+	{"",	"clock",			60,	1},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//sets delimeter
 static char delim = '|';
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }

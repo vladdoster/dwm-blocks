9af31c3d7a560a126f4071b3a7444e08965481e3
Vlad Doster <mvdoster@gmail.com>
2020-07-02 10:59:07 -0500

    Cleanup

--- config.h
+++ config.h
@@ -1,15 +1,21 @@
+/*
+ * Author: Vlad Doster <mvdoster@gmail.com>
+ * Date: 2020-07-02 10:53:04
+ * Last Modified by: Vlad Doster <mvdoster@gmail.com>
+ * Last Modified time: 2020-07-02 10:58:45
+ */
+
+static char delim = '|';
 static const Block blocks[] = {
-	/*Icon*/     /*Command*/      /*Update Interval*/     /*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	{"",	"pacpackages",			0,	8},
-	{"",	"network_traffic",		1,	16},
-	{"",	"battery | tr \'\n\' \' \'",	5,	3},
-	{"",	"memory",			10,	14},
-	{"",	"cpu",				10,	13},
-	{"",	"internet_connection",		5,	4},
-	{"",	"volume",			0,	10},
-	{"",	"clock",			60,	1},
+/*icon,         command,                   interval   signal*/
+  {"", "cat /tmp/recordingicon 2>/dev/null",  0,        9},
+  {"", "upgradeable_pkgs",                    0,        8},
+  {"", "network_usage",                       1,       16},
+  {"", "battery | tr \'\n\' \' \'",           5,        3},
+  {"", "ram_usage",                           10,      14},
+  {"", "cpu_average_usage",                   10,      13},
+  {"", "internet_connection",                 5,        4},
+  {"", "volume",                              0,       10},
+  {"", "clock",                               60,       1},
 };
 
-//sets delimeter
-static char delim = '|';
